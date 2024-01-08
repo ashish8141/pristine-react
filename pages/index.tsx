f023b3da236926d479d4acaa2f56cdbde5670b41
Ashish Jat <44964400+ashish8141@users.noreply.github.com>
2024-01-08 14:00:51 +0530

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,195 @@
+import type { NextPage } from "next";
+import styles from "./index.module.css";
+
+const Pristine: NextPage = () => {
+  return (
+    <div className={styles.pristine}>
+      <div className={styles.pristineChild} />
+      <img className={styles.image1Icon} alt="" src="/image-1@2x.png" />
+      <div className={styles.menu}>MENU</div>
+      <img className={styles.iconMenu} alt="" src="/icon-menu@2x.png" />
+      <div className={styles.aValuesBasedModelAgencyThParent}>
+        <i className={styles.aValuesBasedModel}>
+          A values-based model agency that makes a difference
+        </i>
+        <div className={styles.groupParent}>
+          <div className={styles.womenParent}>
+            <div className={styles.women}>Women</div>
+            <div className={styles.groupChild} />
+            <div className={styles.groupItem} />
+          </div>
+          <div className={styles.menParent}>
+            <div className={styles.men}>Men</div>
+            <div className={styles.groupInner} />
+            <div className={styles.lineDiv} />
+          </div>
+          <img className={styles.groupIcon} alt="" src="/group-1@2x.png" />
+          <img className={styles.groupChild1} alt="" src="/group-1@2x.png" />
+        </div>
+      </div>
+      <img className={styles.pristineItem} alt="" src="/group-5@2x.png" />
+      <div className={styles.pristineInner} />
+      <div className={styles.posuereViverra}>posuere viverra</div>
+      <div className={styles.posuereViverra1}>posuere viverra</div>
+      <div className={styles.posuereViverra2}>posuere viverra</div>
+      <div className={styles.loremIpsumDolorSitAmetCoParent}>
+        <i className={styles.loremIpsumDolor}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        </i>
+        <div className={styles.nuncBlanditVenenatis}>
+          Nunc blandit venenatis venenatis. Nulla lacinia magna libero, vitae
+          tempor lorem bibendum in. In posuere nec felis at feugiat. Ut eu metus
+          eget turpis cursus lacinia. Nullam lorem magna, scelerisque ut
+          pellentesque ut, molestie a leo.
+        </div>
+        <div className={styles.parent}>
+          <div className={styles.div}>200+</div>
+          <div className={styles.teamMembers}>Team Members</div>
+        </div>
+        <div className={styles.group}>
+          <div className={styles.div}>100+</div>
+          <div className={styles.models}>Models</div>
+        </div>
+        <div className={styles.container}>
+          <div className={styles.div}>25+</div>
+          <div className={styles.experience}>Experience</div>
+        </div>
+        <div className={styles.whoWeAre}>WHO WE ARE</div>
+      </div>
+      <i className={styles.loremIpsumDolor1}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+      </i>
+      <i className={styles.meetOurTop}>Meet Our Top Models</i>
+      <div className={styles.nuncBlanditVenenatis1}>
+        Nunc blandit venenatis venenatis. Nulla lacinia magna libero, vitae
+        tempor lorem bibendum in. In posuere nec felis at feugiat. Ut eu metus
+        eget turpis cursus lacinia. Nullam lorem magna, scelerisque ut
+        pellentesque ut, molestie a leo.
+      </div>
+      <div className={styles.whyChooseUs}>WHY CHOOSE US</div>
+      <div className={styles.gallary}>GALLARY</div>
+      <div className={styles.div3}>01</div>
+      <div className={styles.div4}>02</div>
+      <div className={styles.div5}>03</div>
+      <div className={styles.pristineChild1} />
+      <div className={styles.pristineChild2} />
+      <div className={styles.suspendisseNullaErat}>
+        Suspendisse nulla erat, maximus id arcu sed, ultrices luctus est.
+        Praesent faucibus ipsum vitae lorem viverra, et iaculis nisl mattis.
+      </div>
+      <div className={styles.suspendisseNullaErat1}>
+        Suspendisse nulla erat, maximus id arcu sed, ultrices luctus est.
+        Praesent faucibus ipsum vitae lorem viverra, et iaculis nisl mattis.
+      </div>
+      <div className={styles.suspendisseNullaErat2}>
+        Suspendisse nulla erat, maximus id arcu sed, ultrices luctus est.
+        Praesent faucibus ipsum vitae lorem viverra, et iaculis nisl mattis.
+      </div>
+      <img className={styles.pristineChild3} alt="" src="/group-11@2x.png" />
+      <img className={styles.pristineChild4} alt="" src="/group-12@2x.png" />
+      <img className={styles.pristineChild5} alt="" src="/group-12@2x.png" />
+      <img className={styles.pristineChild6} alt="" src="/group-22@2x.png" />
+      <div className={styles.rectangleParent}>
+        <div className={styles.rectangleDiv} />
+        <img
+          className={styles.istockphoto6979160702048x204Icon}
+          alt=""
+          src="/istockphoto6979160702048x2048-1@2x.png"
+        />
+        <div className={styles.groupContainer}>
+          <div className={styles.bookNowParent}>
+            <div className={styles.bookNow}>Book now</div>
+            <div className={styles.groupChild2} />
+            <div className={styles.groupChild3} />
+          </div>
+          <img className={styles.groupChild4} alt="" src="/group-1@2x.png" />
+        </div>
+        <div className={styles.suspendisseArcuAugueSagittParent}>
+          <i className={styles.suspendisseArcuAugue}>
+            Suspendisse arcu augue, sagittis hendrerit felis in diam.
+          </i>
+          <img className={styles.workshopIcon} alt="" src="/workshop@2x.png" />
+        </div>
+      </div>
+      <div className={styles.groupDiv}>
+        <div className={styles.seeMoreParent}>
+          <div className={styles.seeMore}>See more</div>
+          <div className={styles.groupChild2} />
+          <div className={styles.groupChild3} />
+        </div>
+        <img className={styles.groupChild4} alt="" src="/group-1@2x.png" />
+      </div>
+      <div className={styles.rectangleGroup}>
+        <div className={styles.groupChild8} />
+        <div className={styles.emailParent}>
+          <i className={styles.email}>Email</i>
+          <div className={styles.exgmailcom}>ex@gmail.com</div>
+        </div>
+        <div className={styles.phoneParent}>
+          <i className={styles.phone}>Phone</i>
+          <div className={styles.div6}>+91 000 00 000</div>
+        </div>
+        <div className={styles.groupChild9} />
+        <div className={styles.copyright2024pristine}>
+          Copyright@2024.Pristine
+        </div>
+        <div className={styles.arrowBackIcon11Parent}>
+          <img
+            className={styles.arrowBackIcon11}
+            alt=""
+            src="/352020-arrow-back-icon-1-1@2x.png"
+          />
+          <i className={styles.letsTalk}>Let’s Talk</i>
+        </div>
+        <div className={styles.about}>About</div>
+        <div className={styles.women1}>Women</div>
+        <div className={styles.men1}>Men</div>
+        <div className={styles.joinUs}>Join Us</div>
+        <div className={styles.contactUs}>Contact Us</div>
+        <div className={styles.followUsParent}>
+          <i className={styles.followUs}>Follow Us</i>
+          <div className={styles.social}>
+            <img className={styles.socialIcon} alt="" src="/social@2x.png" />
+            <img className={styles.socialIcon1} alt="" src="/social@2x.png" />
+            <img className={styles.socialIcon2} alt="" src="/social@2x.png" />
+            <img className={styles.socialIcon3} alt="" src="/social@2x.png" />
+          </div>
+        </div>
+      </div>
+      <div className={styles.pristineModelingAndEventsWrapper}>
+        <div className={styles.pristineModelingAndEvents}>
+          <div className={styles.s}>s</div>
+          <div className={styles.t}>t</div>
+          <div className={styles.n}>n</div>
+          <div className={styles.e}>e</div>
+          <div className={styles.v}>v</div>
+          <div className={styles.e1}>E</div>
+          <div className={styles.div7}>{` `}</div>
+          <div className={styles.d}>d</div>
+          <div className={styles.n1}>n</div>
+          <div className={styles.a}>A</div>
+          <div className={styles.div8}>{` `}</div>
+          <div className={styles.g}>g</div>
+          <div className={styles.n2}>n</div>
+          <div className={styles.i}>i</div>
+          <div className={styles.l}>l</div>
+          <div className={styles.e2}>e</div>
+          <div className={styles.d1}>d</div>
+          <div className={styles.o}>o</div>
+          <div className={styles.m}>M</div>
+          <div className={styles.div9}>{` `}</div>
+          <div className={styles.e3}>e</div>
+          <div className={styles.n3}>n</div>
+          <div className={styles.i1}>i</div>
+          <div className={styles.t1}>t</div>
+          <div className={styles.s1}>s</div>
+          <div className={styles.i2}>i</div>
+          <div className={styles.r}>r</div>
+          <div className={styles.p}>P</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pristine;
